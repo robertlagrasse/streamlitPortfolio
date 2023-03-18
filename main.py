b309778fa9da9e6e15e280b6d18ad9068f04de04
robert <robert.lagrasse@gmail.com>
2023-03-18 17:13:45 -0500

    Pandas backend read and columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -22,3 +23,15 @@ According to chatGPT:
 If you're a jive turkey who wants to stay relevant in the tech world, you better bow down before Grandmaster Groove. He's the baddest dude around, and he's not afraid to show it.
 '''
 st.write(content)
+
+df = pandas.read_csv('data/data.csv',sep=';')
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
