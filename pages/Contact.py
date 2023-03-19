9d0c26bbb1240aa05808412ca5cc76bfa8cb82b5
robert <robert.lagrasse@gmail.com>
2023-03-19 14:40:23 -0500

    Tie front and back end together. Bring in topics.csv for email subjects Bring in credentials from file, including host and port information

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,12 +1,28 @@
 import streamlit as st
+import sendMail
+import pandas
+
+df=pandas.read_csv('data/topics.csv')
+signature = '''
+Brought to you by Grandmaster Groove
+Read these lines and shut yo mouth.
+    '''
+
 st.set_page_config(layout="wide")
 st.header('Contact Me')
 
-with st.form(key='form'):
+with st.form(key='form', clear_on_submit=True):
     email = st.text_input("Your Email Address")
-    message = st.text_area("Your Message...")
+    subject = f"Subject:{st.selectbox('Subject', df['topic'])}\n"
+    raw_message = st.text_area("Your Message...")
     submit = st.form_submit_button("Submit")
-    if submit:
-        st.warning("Submit button pressed. ")
 
-ubqkhrqcudmavslg
+    header = f"A new webform submission from {email} has arrived\n"
+    message = subject + \
+              header + \
+              raw_message + \
+              signature
+
+    if submit:
+        sendMail.send(email, message)
+        st.info("Your email was sent!")
--- sendMail.py
+++ sendMail.py
@@ -2,23 +2,16 @@ import smtplib
 import ssl
 import json
 
-host = 'smtp.gmail.com'
-port = 465
+def send(receiver, message):
+    with open('data/creds.json', 'r') as file:
+        creds = json.loads(file.read())
+        username = creds['username']
+        password = creds['password']
+        host = creds['host']
+        port = creds['port']
 
-with open('data/creds.json', 'r') as file:
-    creds = json.loads(file.read())
-    username = creds['username']
-    password = creds['password']
+        context = ssl.create_default_context()
 
-receiver = 'robert.lagrasse@gmail.com'
-context = ssl.create_default_context()
-
-message = '''
-This is a test
-Don't be alarmed.
-This is only a test.
-'''
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
